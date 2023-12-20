bfbe3eafde236058d962c93ee233890bcbfcd122
truong phuc <truongngo1309@gmail.com>
2023-12-20 22:11:01 +0700

    fix bug

--- src/components/main/handlers/customChatengine.js
+++ src/components/main/handlers/customChatengine.js
@@ -10,34 +10,43 @@ const customize = () => {
     const settingsBtnM = document.querySelector('.ce-mobile-chat-settings-button');
     settingsBtnM.innerHTML = '<i class="fa-solid fa-bars"></i>';
 
-    const messes = document.querySelectorAll('.ce-message-list [id^="ce-message"]');
-    const lastMess = messes[messes.length - 1];
-    const lastMessContent = lastMess.querySelector('.ce-my-message-body');
-    const deleteEmtyMess = async () => {
-        const chatList = document.querySelector('[id^="ce-message-list"]');
-        const chatIdArr = chatList.id.split('-');
-        const chatId = chatIdArr[chatIdArr.length - 1];
-        const messIdArr = lastMess.id.split('-');
-        const messId = messIdArr[messIdArr.length - 1];
-        await ChatEngineApi.deleteMessage(chatId, messId);
-    }
-    if (lastMessContent)
-    {
-        const haveFile = lastMess.querySelector('.ce-my-message-files .ce-message-file');
-        const haveImg = lastMess.querySelector('.ce-my-message-images img');
-        console.log(haveFile);
-        console.log(haveImg);
-        if (lastMessContent.innerHTML === ''){
-            if(!haveFile && !haveImg){
+    try {
+        const messes = document.querySelectorAll('.ce-message-list [id^="ce-message"]');
+        const lastMess = messes[messes.length - 1];
+        const lastMessContent = lastMess.querySelector('.ce-my-message-body');
+        const deleteEmtyMess = async () => {
+            const chatList = document.querySelector('[id^="ce-message-list"]');
+            const chatIdArr = chatList.id.split('-');
+            const chatId = chatIdArr[chatIdArr.length - 1];
+            const messIdArr = lastMess.id.split('-');
+            const messId = messIdArr[messIdArr.length - 1];
+            await ChatEngineApi.deleteMessage(chatId, messId);
+        }
+        if (lastMessContent)
+        {
+            const haveFile = lastMess.querySelector('.ce-my-message-files .ce-message-file');
+            const haveImg = lastMess.querySelector('.ce-my-message-images img');
+            console.log(haveFile);
+            console.log(haveImg);
+            if (lastMessContent.innerHTML === '' && !haveFile && !haveImg){
                 lastMess.style.display = 'none';
                 deleteEmtyMess();
-            }else{
-                const emtyMess = lastMess.querySelector('.ce-my-message-body').parentElement;
-                const theirEmtyMess = lastMess.querySelector('.ce-their-message-body').parentElement
-                emtyMess.style.display = 'none';
-                theirEmtyMess.style.display = 'none';
             }
         }
+    
+        const theirMesses = document.querySelectorAll('.ce-their-message-body');
+        for (let theirMess of theirMesses){
+            theirMess.style.display = 'none';
+        }
+
+        const myMesses = document.querySelectorAll('.ce-my-message-body');
+        for (let myMess of myMesses){
+            myMess.style.display = 'none';
+        }
+
+
+    }catch{
+
     }
 
     const isMobile = document.querySelector('.ce-chat-engine > .ce-chat-list');
