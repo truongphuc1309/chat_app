2657bae532483d0c23b6fdf7b7cf2fb5011bb8b6
truong phuc <truongngo1309@gmail.com>
2024-01-18 15:57:08 +0700

    fix bug

--- src/components/main/elements/chatengine.js
+++ src/components/main/elements/chatengine.js
@@ -7,7 +7,7 @@ function ChatEgine (props) {
 
     useEffect(customize);
 
-    const chatProps = useMultiChatLogic('63b00dfb-0d32-4308-96dc-c5a0b72ef2a5',props.user, '@@@###$$$');
+    const chatProps = useMultiChatLogic('44a0f3c3-e5ed-4067-ac30-c41eac813737',props.user, '@@@###$$$');
 
     return (     
         <div style = {{height : '100vh'}}>
